2420b41743ecb44a93b1eaff1f0e012028fa2239
boombuster2000 <chanuka.williams@outlook.com>
2022-12-12 18:17:55 +0000

    fixed hash disapearing bug

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <time.h>
 #include <string>
 #include <conio.h>
+#include <Windows.h>
+#include <iomanip>
 
 #define set_text_red std::cout << "\033[0;31m"
 #define set_text_green std::cout << "\033[0;32m"
@@ -15,12 +17,30 @@ struct Coords {
 	short y;
 };
 
+void increase_text_size() {
+	static CONSOLE_FONT_INFOEX  fontex;
+	fontex.cbSize = sizeof(CONSOLE_FONT_INFOEX);
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetCurrentConsoleFontEx(hOut, 0, &fontex);
+	fontex.FontWeight = 700;
+	fontex.dwFontSize.X = 36;
+	fontex.dwFontSize.Y = 36;
+	SetCurrentConsoleFontEx(hOut, NULL, &fontex);
+}
 
 void print_board(char board[3][3]) {
 	system("cls");
+	short space = 52;
 	set_text_red;
+
+	//for (short lines_down = 0; lines_down < 10; lines_down++) {
+	//	std::cout << std::endl;
+	//}
+	
+	//std::cout << std::setw(space);
 	std::cout << "|";
 	for (short y = 0; y < 3; y++) {
+		
 		for (short x = 0; x < 3; x++) {
 			if (board[y][x] == '#') set_text_green;
 			else set_text_cyan;
@@ -28,25 +48,30 @@ void print_board(char board[3][3]) {
 			set_text_red;
 			std::cout << "|";
 			
-			
 		}
-		if (y != 2) std::cout << std::endl << "|";
+		
+		if (y != 2) {
+			//std::cout << std::setw(space);
+			std::cout << std::endl << "|";
+		}
+		
 	}
 	std::cout << std::endl;
 }
 
-Coords pick_random_square(Coords squares_filled[3]) {
+Coords pick_random_square(char board[3][3]) {
 	Coords coords;
 	coords.x = rand() % 3;
 	coords.y = rand() % 3;
 	//makes sure not already picked
-	for (short i = 0; i < 3; i++) {
-		if (coords.x == squares_filled[i].x && coords.y == squares_filled[i].y) {
-			i = 0;
+	bool picked = false;
+	do {
+		if (board[coords.y][coords.x] == '#') {
 			coords.x = rand() % 3;
 			coords.y = rand() % 3;
 		}
-	}
+		else picked = true;
+	} while (!picked);
 	return coords;
 }
 
@@ -56,23 +81,13 @@ Coords user_move(char board[3][3]) {
 		{'4','5','6'},
 		{'1','2','3' } };
 
-	bool valid_input = false;
 	char user_square;
-	do {
+
+	while (true) {
 		user_square = _getch();
 		for (int y = 0; y < 3; y++) {
 			for (int x = 0; x < 3; x++) {
-				if (user_square == board_template[y][x]) {
-					valid_input = true;
-				}
-			}
-		}
-	} while (!valid_input);
-	
-	for (short y = 0; y < 3; y++) {
-		for (short x = 0; x < 3; x++) {
-			if (board_template[y][x] == user_square) {
-				if (board[y][x] == '#') {
+				if (user_square == board_template[y][x] && board[y][x] == '#') {
 					Coords position;
 					position.x = x;
 					position.y = y;
@@ -89,6 +104,7 @@ int main() {
 		{'O','O','O'},
 		{'O','O','O' } };
 
+	increase_text_size();
 	srand(time(0));
 	print_board(board);
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
@@ -96,7 +112,7 @@ int main() {
 	// pickking 3 random squares to start
 	Coords squares_filled[3];
 	for (short i = 0; i < 3; i++) {
-		squares_filled[i] = pick_random_square(squares_filled);
+		squares_filled[i] = pick_random_square(board);
 		board[squares_filled[i].y][squares_filled[i].x] = '#';
 	}
 
@@ -106,17 +122,20 @@ int main() {
 	while (true) {
 		
 		Coords coords = user_move(board);
-		
-		board[coords.y][coords.x] = 'O';
 
 		//removes index
 		for (short i = 0; i < 3; i++) {
 			if (squares_filled[i].x == coords.x && squares_filled[i].y == coords.y) {
-				squares_filled[i] = pick_random_square(squares_filled);
+				squares_filled[i] = pick_random_square(board);
 				board[squares_filled[i].y][squares_filled[i].x] = '#';
+				/*std::cout << "Y: " << squares_filled[i].y << std::endl;
+				std::cout << "X: " << squares_filled[i].x << std::endl;
+				system("pause");*/
 				score++;
 			}
 		}
+
+		board[coords.y][coords.x] = 'O';
 		print_board(board);
 	}
 
